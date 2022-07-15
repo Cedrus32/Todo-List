d16ad6e068ae78342a851c4eac4f38924986c2cb
Shannon <69023504+Cedrus32@users.noreply.github.com>
2022-07-14 19:44:49 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import './reset.css';
+import Img from './img.png';
+import XmlData from './data.xml';
+import CsvData from './data.csv';
+
+console.log(Img);
+console.log(XmlData);
+console.log(CsvData);
+
+// manager function here
+
+// ...
