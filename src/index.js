99d19273146b553cdecec6384d5b8318caf1d68e
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-07-14 23:33:20 -0500

    Make task factory

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './styles/typo.css';
 import './styles/shaping.css';
 import './styles/colors.css';
 
+import library from './scripts/library'
+
 // manager function here
 
-// ...
+let viewPrefs = document.getElementById('view-prefs');
+let title = document.getElementById('title');
+title.addEventListener('click', () => {
+    viewPrefs.classList.toggle('hide');
+});
+
+// qc
+let task1 = library.createTask('project', 'type', 'title', 'desc', 'date', 'priority', 'tag')
+console.log(task1);
+console.log('');
+library.viewLibArray();
+console.log('');
---
+++ src/scripts/library.js
@@ -0,0 +1,92 @@
+const library = (() => {
+    // data
+    let _libArray = [];
+    let _taskCounter = 1;
+
+    //// cache DOM
+
+    // bind events
+
+    // task factory
+    const task = function (id, projectName, type, title, description, dueDate, priority, tags) {
+        let _id = id;
+        let _project = projectName;
+        let _type = type;
+        let _title = title;
+        let _description = description;
+        let _dueDate = dueDate;
+        let _priority = priority;
+        let _tags = tags;
+
+        // methods
+        const modTitle = (title) => {
+            _title = title;
+        }
+        const modDesc = (description) => {
+            _desc = description;
+        }
+        const modDate = (dueDate) => {
+            _dueDate = dueDate;
+        }
+        const modPriority = (priority) => {
+            _priority = priority;
+        }
+        const modTags = (tags) => {
+            _tags = tags;
+        }
+
+        // helpers
+        const viewInfo = () => {
+            console.log(_id);
+            console.log(_project);
+            console.log(_type);
+            console.log(_title);
+            console.log(_description);
+            console.log(_dueDate);
+            console.log(_priority);
+            console.log(_tags);
+        }
+
+        return {
+            modTitle,
+            modDesc,
+            modDate,
+            modPriority,
+            modTags,
+            viewInfo,
+        }
+    }
+
+    // managers
+
+    // methods
+    function createTask(projectName, type, title, description, dueDate, priority, tags) {
+        let id = _taskCounter;
+        let newTask = task(id, projectName, type, title, description, dueDate, priority, tags);
+        _libArray.push(newTask);
+        _taskCounter++;
+        return newTask;
+    }
+    function deleteTask() {
+        // index into libArray, delete task
+    }
+
+    // helpers
+    function viewLibArray() {
+        for (let t in _libArray) {
+            _libArray[t].viewInfo();
+        };
+    }
+
+    // actions
+
+    // make public
+    return {
+        createTask,
+        deleteTask,
+        viewLibArray,
+    };
+
+})();
+
+export default library;
