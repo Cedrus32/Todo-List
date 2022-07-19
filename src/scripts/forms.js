a8fb61a137d2a77691a5e5bd7790a148d327f75c
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-07-19 06:52:36 -0500

    Close form on cancel click

--- src/scripts/forms.js
+++ src/scripts/forms.js
@@ -4,32 +4,57 @@ import library from './library';
 
 const forms = (() => {
     // data
+    let _currentForm;
 
     // cache DOM
     let projForm = document.getElementById('proj-form');
     let taskForm = document.getElementById('task-form');
+    let confirmBtns = document.querySelectorAll('button.confirm');
+    let cancelBtns = document.querySelectorAll('button.cancel');
 
     // bind events
+    // confirmBtns.forEach(btn => btn.addEventListener('click', () => {
+    //     // call _confirmInput();
+    // }));
+    cancelBtns.forEach(btn => btn.addEventListener('click', () => {
+        _cancelInput();
+    }));
 
     // factories
 
     // manager methods
     function openCreate(formType) {
-        showForm(formType);
+        _showForm(formType);
     }
     function openModify(formType) {
         // query library
         // populate fields
-        showForm(formType);
+        _showForm(formType);
+    }
+    function _confirmInput() {
+        _hideForm();
+        // push new to library OR modify library item
+        // clear input values
+
+    }
+    function _cancelInput() {
+        _hideForm();
+        // clear input values
     }
 
     // helper methods
-    function showForm(formType) {
+    function _showForm(formType) {
         if (formType === 'proj') {
+            _currentForm = projForm;
             projForm.classList.remove('hide');
         } else if (formType === 'task') {
+            _currentForm = taskForm;
             taskForm.classList.remove('hide');
         };
+    }
+    function _hideForm() {
+        _currentForm.classList.add('hide');
+        _currentForm = '';
     }
 
     return {
