30d44814dfecc5b3491ee9c06accfe8fb6a8ad17
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-07-21 18:23:29 -0500

    Create pubsub module to handle communications

---
+++ src/events.js
@@ -0,0 +1,37 @@
+const events = (() => {
+    let _events = {};
+
+    // subscribe event to list
+    function subscribe(eventName, callback) {
+        if (!Array.isArray(_events[eventName])) {
+            _events[eventName] = [];
+        };
+        _events[eventName].push(callback);
+    }
+
+    // unsubscribe event to list
+    function unsubscribe(eventName) {
+        console.log(eventName);
+        console.log(_events[eventName].length - 1);
+        _events[eventName].splice((_events[eventName].length - 1), 1);
+    }
+
+    // publish event with data
+    function publish(eventName, data) {
+        if (!Array.isArray(_events[eventName])){
+            return;
+        }
+        _events[eventName].forEach(callback => {
+            callback(data);
+        });
+    }
+
+    return {
+        on,
+        off,
+        emit,
+    }
+
+})();
+
+export default events;
