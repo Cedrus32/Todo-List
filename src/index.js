938a3671cf1eb08be51483ccfc764d016daa2df9
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-10-02 06:38:59 -0500

    Import forms styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/reset.css';
 import './styles/spacing.css';
 import './styles/typo.css';
+import './styles/forms.css';
 import './styles/effects.css';
 import './styles/colors.css';
 
